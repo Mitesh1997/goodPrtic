d1c65bd2f562881aa23a9796e3555b1880e692f6
Your Name <you@example.com>
2024-05-12 18:52:38 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,67 @@
+import express from 'express'
+
+const app= express()
+
+app.get('/api/products',(req,res)=>{
+
+    const products=[
+        {
+        "userId": 1,
+        "id": 1,
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+        "userId": 1,
+        "id": 2,
+        "title": "qui est esse",
+        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        },
+        {
+        "userId": 1,
+        "id": 3,
+        "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+        },
+        {
+        "userId": 1,
+        "id": 4,
+        "title": "eum et est occaecati",
+        "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+        },
+        {
+        "userId": 1,
+        "id": 5,
+        "title": "nesciunt quas odio",
+        "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+        },
+        {
+        "userId": 1,
+        "id": 6,
+        "title": "dolorem eum magni eos aperiam quia",
+        "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+        },
+    ]
+    
+    if(req.query.search){
+        const filterProduct= products.filter(product=>
+            product.title.includes(req.query.search))
+     res.send(filterProduct)    
+     return;
+ }
+
+    setTimeout(() => {
+        // http://localhost:3001/api/products?search=metal
+       
+        res.send(products)
+
+
+    }, 2*1000);
+
+})
+
+
+const port=process.env.PORT || 3001;
+app.listen(port,()=>{
+console.log(`Server running on port ${port}`);
+ })
--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Custonhook from "./Custonhook";
 
 function App() {
+  const [search,setSeacrch]=useState("")
+  // const urlPath="http://localhost:3001/api/products?search="+search;
+//  const [products,error,loading]= Custonhook("http://localhost:3001/api/products?search="+search)
+ 
+// 
+const [products, setProducts] = useState([]);
+const [error, setError] = useState(null)
+const [loading, setLoading] = useState(null)
+useEffect(() => {
+  const controller=new AbortController()
+  ;(async () => {
+  try {
+    setError(false)
+    setLoading(true)
+    
+    const response = await axios.get("http://localhost:3001/api/products?search="+search,{
+      signal:controller.signal
+    });
+    console.log(response.data)
+    setProducts(response.data)
+    setLoading(false)
+  }
+    catch(error){
+      if(axios.isCancel(error)){
+        console.log("Req cancel",error.message);
+        return
+      }
+      setError(true)
+      setLoading(false)
+    }
+  })();
+  return ()=>{
+    controller.abort()
+  }
+}, [search]);
+// 
+
+  if(error){
+    return <h1>Something went wrong</h1>
+  }
+  if(loading){
+    return <h1>Loading .....</h1>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <input type="text" placeholder="search"
+    value={search}
+    onChange={(e)=>setSeacrch(e.target.name)}
+    
+    /> */}
+    <input 
+  type="text" 
+  placeholder="search"
+  value={search}
+  onChange={(e) => setSeacrch(e.target.value)}
+/>
+      <h1>Number of product are : {products.length}</h1>
+    </>
+    
   );
+
+
+  // return (
+  //   <>
+  //     {error && <h1>Something went wrong</h1>}
+  //     {loading && <h1>Loading .....</h1>}
+  //     {!error && !loading && (
+  //       <h1>Number of products are: {products.length}</h1>
+  //     )}
+  //   </>
+  // );
+  
 }
 
-export default App;
+export default App;   
+  
